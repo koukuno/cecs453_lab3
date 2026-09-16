489cb1222cfab6199f5f63120926f499ade248e3
Toby Moeung <tobym562@protonmail.com>
2026-09-16 14:59:49 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cecs453_lab3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cecs453_lab3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AffirmationsApp());
+}
+
+class AffirmationsApp extends StatelessWidget {
+  const AffirmationsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Affirmations",
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark),
+      ),
+      home: const AffirmationsHomePage(),
+    );
+  }
+}
+
+class AffirmationCard extends StatelessWidget {
+  final String imagePath;
+  final String message;
+
+  const AffirmationCard({super.key, required this.imagePath, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: Column(children: [
+      Image(image: AssetImage(imagePath)),
+      Text(message),
+    ]));
+  }
+}
+
+class AffirmationsHomePage extends StatelessWidget {
+  const AffirmationsHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var cards = <AffirmationCard>[];
+    for (var i = 0; i < 10; ++i) {
+      cards.add(AffirmationCard(imagePath: "assets/test.png", message: "Test Message $i"));
+    }
+
+    return ListView(children: cards);
+  }
+}
